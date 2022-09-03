b2144d84514db3cb373c538ceb09ad097d135d80
Gerson <gerson.marihuan@usm.cl>
2022-09-02 21:15:16 -0400

    Readme and Author

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# HurryBoats
-Simple multiplayer mini-game using steam api.
+# HurryBoats (Prototype)
+
+![image info](readme\Godot2022.09.02.jpg)
+
+This is a (2-4) peer-to-peer game, where you are driving
+a boat and your goal is to be the last one alive. Pick the cannonball crates and defeat the other players. Try to avoid the edge of the map and the cannonballs from another players, otherwise you will lose.
+
+# Controls
+
+- Select your ammunition with Q and E
+- Shoot your current ammmo with SPACE
+- Move with WASD
+
+# Requirements
+- Godot 3.5 mono version (c# support)
+- Steam open
+- .Net SDK 4.7.2 
+
+# Intructions
+
+To debug this project you have to change in your editor settings the build:
+
+Editor -> Editor Settings -> Mono -> Build -> change to dotcli.
--- scenes/main_menu/MainMenu.cs
+++ scenes/main_menu/MainMenu.cs
@@ -4,34 +4,34 @@ using System;
 public class MainMenu : Control
 {
 
-    SceneChanger scene_changer;
-    LobbyManager lobby_manager;
+	SceneChanger scene_changer;
+	LobbyManager lobby_manager;
 
-    public override void _Ready(){
-        lobby_manager = GetNode<LobbyManager>("/root/LobbyManager");
-        scene_changer = GetNode<SceneChanger>("/root/SceneChanger");
-        lobby_manager.Connect("LobbyCreatedResult", this, nameof(_OnLobbyCreatedResult) );
-        
-    }
+	public override void _Ready(){
+		lobby_manager = GetNode<LobbyManager>("/root/LobbyManager");
+		scene_changer = GetNode<SceneChanger>("/root/SceneChanger");
+		lobby_manager.Connect("LobbyCreatedResult", this, nameof(_OnLobbyCreatedResult) );
+		
+	}
 
-    void HostGame(){
-        lobby_manager.CreateLobby(); 
-    }
-
-
-    public void _OnLobbyCreatedResult(bool result){
-        if(result){
-            scene_changer.ChangeSceneTo(this, SceneChanger.SCENES.LOBBY);
-        }
-    }
+	void HostGame(){
+		lobby_manager.CreateLobby(); 
+	}
 
 
-    public void _OnStartGamePressed(){
-        HostGame();
-    }
+	public void _OnLobbyCreatedResult(bool result){
+		if(result){
+			scene_changer.ChangeSceneTo(this, SceneChanger.SCENES.LOBBY);
+		}
+	}
 
 
-    public void _OnJoinGamePressed(){
-        scene_changer.ChangeSceneTo(this, SceneChanger.SCENES.JOIN_LOBBY);
-    }
+	public void _OnStartGamePressed(){
+		HostGame();
+	}
+
+
+	public void _OnJoinGamePressed(){
+		scene_changer.ChangeSceneTo(this, SceneChanger.SCENES.JOIN_LOBBY);
+	}
 }
